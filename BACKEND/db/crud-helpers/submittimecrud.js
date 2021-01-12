b7e6a3b5cfa938657334c9cacfc9a5a5ff662ef3
Vidit <43810533+gv15@users.noreply.github.com>
2021-01-12 21:49:49 +0530

    Add files via upload

--- BACKEND/db/crud-helpers/submittimecrud.js
+++ BACKEND/db/crud-helpers/submittimecrud.js
@@ -5,6 +5,13 @@ const submitTimestampCrud = {
     },
     checkIfAlreadyStamped(user_id){
         return SubmitTimestampModel.findOne({u_id:user_id});
+    },
+    async giveLeftTime(user_id){
+      await  this.checkIfAlreadyStamped(user_id).then(stamp=>{
+            console.log("Stamp", stamp);
+            stamp = stamp.toObject();
+            return stamp.timeStamp-Date.now();
+        })
     }
 }
 module.exports = submitTimestampCrud;
--- BACKEND/routes/problemRoute.js
+++ BACKEND/routes/problemRoute.js
@@ -10,41 +10,41 @@ problemRouter.get("/", (req, res) => {
 problemRouter.get("/all", (req, res) => {
     let user = tokenOperations.validate(req.headers.bearer);
     if (user.isTokenValid) {
-        const questions = quesCrud.giveAll();
-        questions.then((questions) => {
-           
+        const questionsPromise = quesCrud.giveAll();
+        questionsPromise.then((questions) => {
+
             submitTimeCrud.checkIfAlreadyStamped(user['user_id']).then(
-                (stamp)=>{
-                    
-                    if(stamp){
-                        
-                        if(stamp.timeStamp-Date.now()>0){
+                (stamp) => {
+
+                    if (stamp) {
+
+                        if (stamp.timeStamp - Date.now() > 0) {
                             console.log("Test Started")
-                           let testTime = stamp.timeStamp-Date.now();
-                           
-                           questions.push(testTime);
-                           res.status(200).json(questions);
+                            let testTime = stamp.timeStamp - Date.now();
+
+                            questions.push(testTime);
+                            res.status(200).json(questions);
                         }
-                        else{
+                        else {
                             res.status(403).json({
-                                message:"Test Time Out"
+                                message: "Test Time Out"
                             })
                         }
                     }
-                    else{
-                       
+                    else {
+
                         let testTime = utils.calculateTestTime(questions);
                         questions.push(testTime);
-                        let endTimeStamp = Date.now()+testTime+90000;
+                        let endTimeStamp = Date.now() + testTime + 90000;
                         submitTimeCrud.registerStamp({
-                            u_id:user.user_id,
-                            timeStamp:endTimeStamp
+                            u_id: user.user_id,
+                            timeStamp: endTimeStamp
                         })
                         res.status(200).json(questions);
                     }
                 }
             )
-           
+
         }).catch(() => {
 
             res.json({ message: "Error Fetching Questions" })
@@ -60,18 +60,32 @@ problemRouter.get("/all", (req, res) => {
 problemRouter.get('/:quesName', (req, res) => {
     let user = tokenOperations.validate(req.headers.bearer);
     if (user.isTokenValid) {
-        console.log("User decoded", user);
         const question = quesCrud.searchByName(req.params.quesName);
         question.then(ques => {
             if (ques) {
                 ques.isTokenValid = true;
-                res.status(200).json(ques);
+                
+                submitTimeCrud.checkIfAlreadyStamped(user["user_id"]).then((stamp) => {
+                    if(stamp){
+                        ques["timeLeft"] = stamp.timeStamp - Date.now();
+                        res.status(200).json(ques);
+                    }
+                    else{
+                        res.status(403).json(
+                            {
+                                message:"Test Not Started"
+                            }
+                        )
+                    }
+                })
             }
             else {
                 res.status(502).json({ message: "Error Fetching Question" });
             }
+
         })
-    }
+        
+}
     else {
         res.status(401).json({
             message: "Unauthorized Request"
